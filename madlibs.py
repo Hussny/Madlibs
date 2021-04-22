52795505e7e527891d450bb07155a7cc04228bc3
Hussein Salat <husseinsalat24@gmail.com>
2021-04-23 02:11:16 +0300

    Defination and Comments

--- madlibs.py
+++ madlibs.py
@@ -1,2 +1,12 @@
 # string concatenation = This is basically how to put strings together.
 # We are going to use the f string to define this.
+
+adj = input ("Adjective: ")
+verb1 = input ("verb: ")
+verb2 = input ("verb:")
+famous_person = input ("Famous Person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because\
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}"
+
+print(madlib)
